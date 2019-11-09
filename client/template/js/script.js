588e51b4935913dbe12fdfeddc086666a281174c
Amit <amit0805kumar@gmail.com>
2019-11-10 00:55:29 +0530

    section-cycles created

--- client/template/js/script.js
+++ client/template/js/script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).on('ready', function () {
 
     var sidebar = $('.sidebar')
     var navbarLinks = $('.nav a.link')
@@ -8,4 +8,15 @@ $(function () {
     sidebar.on('mouseleave', function () {
         navbarLinks.css('color', '#2D2D2D')
     })
+
+
+    //Slider config
+    $(".regular").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 5,
+        slidesToScroll: 3
+    });
+
+
 })
