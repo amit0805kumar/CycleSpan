ac6bf34134299312e376a5a2c83ac9895ca83697
Amit Kumar <32258879+amit0805kumar@users.noreply.github.com>
2020-04-19 22:54:00 +0530

    made deployable

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
-const http = require("http");
+const path = require('path')
 const connectDb = require('./config/db')
-const socketIo = require("socket.io")
-const axios = require("axios");
 const cors = require('cors')
 connectDb();
 
@@ -23,23 +21,20 @@ app.use('/api/rides', require('./routes/api/rides'))
 app.use('/api/activeRide', require('./routes/api/activeRides'))
 app.use('/api/mailer', require('./routes/api/mailer'))
 
+//Serve static assets in production
+if(process.env.NODE_ENV === "production"){
+  //set static folder
+ app.use(express.static('client/build'));
+
+ app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+ });
+}
+
 const PORT = process.env.PORT || 5000
-const server = http.createServer(app);
-const io = socketIo(server) 
 
-// io.on("connection", socket => {
-//     console.log("New client connected")
-//     socket.on("disconnect", () => console.log("Client disconnected"));
-//   });
 
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server started in port: ${PORT}`)
 });
 
-
-
-
-
-app.get('/',function (req, res) {
-  return res.send('API is running')
-})
